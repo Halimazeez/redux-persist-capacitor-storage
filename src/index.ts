5311aba26b43458f2cf0cdf5d6800c0ea5c1eed7
Halim Azeez <halim.azeez@signaturelnd.com>
2019-12-06 15:21:16 +0000

    TypeScript compiled, first version of set/get/clear/readall/delete

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import {
+  Plugins,
+  FilesystemDirectory,
+  FilesystemEncoding
+} from "@capacitor/core";
+
+const { Filesystem } = Plugins;
+export class CapacitorStore {
+  async getItem(key: string) {
+    await Filesystem.readFile({
+      path: key,
+      directory: FilesystemDirectory.Cache,
+      encoding: FilesystemEncoding.UTF8
+    });
+  }
+
+  async setItem(path: string, data: any) {
+    await Filesystem.writeFile({
+      path,
+      data,
+      directory: FilesystemDirectory.Cache,
+      encoding: FilesystemEncoding.UTF8
+    });
+  }
+
+  async removeItem(path: string) {
+    await Filesystem.deleteFile({
+      path,
+      directory: FilesystemDirectory.Cache
+    });
+  }
+
+  async clear() {
+    await Filesystem.rmdir({
+      path: "secrets",
+      directory: FilesystemDirectory.Cache,
+      recursive: false
+    });
+  }
+
+  async getAllKeys() {
+    await Filesystem.readdir({
+      path: "secrets",
+      directory: FilesystemDirectory.Cache
+    });
+  }
+}
+
+export default storage;
